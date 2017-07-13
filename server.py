439d2b41efee8c4febefbe73bc7a1f02eeb2c2fe
xiaojieluo <xiaojieluoff@gmail.com>
2017-07-13 16:35:38 +0800

    some modify

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from sanic import Sanic
-from server.handler.APIHandler import APIHandler
+from server.handler import BaseHandler
 from server import utils, route
 
 def make_app(name=__name__, route=None, settings=None):
@@ -11,13 +11,15 @@ def make_app(name=__name__, route=None, settings=None):
         app.add_route(view.as_view(), url)
 
     # 注册中间件
-    app.middleware(APIHandler.authenticated)
+    app.middleware(BaseHandler.authenticated)
 
     return app
 
 if __name__ == '__main__':
     server_settings = utils.load_conf('server')
     app = make_app(__name__, route=route, settings=server_settings)
+    # Serves files from the static folder to the URL /static
+    app.static('/static', './server/static')
     app.run(host=server_settings.get('host', '127.0.0.1'),
             port=server_settings.get('port', 8888),
             workers=server_settings.get('workers', 1),
